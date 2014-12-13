36d1c07f218b6bc976583a836d53b06fe596753c
italopessoa <italo.pessoa@hotmail.com>
2014-12-13 16:45:00 -0300

    Body structure created
    First part of the spreadsheet content was created. Represented by  the
    class Spreadsheet.

---
+++ SPFactory/SPFactory.Core/Content/Spreadsheet.cs
@@ -0,0 +1,198 @@
+﻿/*
+ * SPFactory is a class library to generate custom spreadsheets using NPOI.
+ * Copyright (C) 2014 Italo Pessoa (italo.pessoa@hotmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using NPOI.HSSF.UserModel;
+using SPFactory.Core.Head;
+using SPFactory.Core.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPFactory.Core.Content
+{
+    /// <summary>
+    /// Class to join all Spreadsheet components
+    /// </summary>
+    public class Spreadsheet
+    {
+        #region Private Members
+
+        private Header _header;
+        private IList<TableHeader> _tableHeaders;
+        private IList<object> _datasource;
+        private string[] _properties;
+        private IList<Spreadsheet> _spreadsheetList;
+        private int _firstCell = 0;
+        private NPOI.HSSF.UserModel.HSSFCellStyle _spanTitleStyle;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Sheet filter header.
+        /// </summary>
+        public Header Header
+        {
+            get { return _header; }
+            set { _header = value; }
+        }
+
+        /// <summary>
+        /// Principal table header.
+        /// </summary>
+        public IList<TableHeader> TableHeaders
+        {
+            get { return _tableHeaders; }
+            set { _tableHeaders = value; }
+        }
+
+        /// <summary>
+        /// List of objects to bind bind with the sheet.
+        /// </summary>
+        public IList<object> Datasource
+        {
+            get { return _datasource; }
+            set { _datasource = value; }
+        }
+
+        /// <summary>
+        /// List of class's properties used to get the value of each Datasource object.
+        /// </summary>
+        public string[] Properties
+        {
+            get { return _properties; }
+            set { _properties = value; }
+        }
+
+        /// <summary>
+        /// Number of rows of principal table header (TableHeader).
+        /// </summary>
+        public int TableHeaderRows
+        {
+            get { return SheetUtil.GetTotalHeaderRows(_tableHeaders); }
+        }
+
+        /// <summary>
+        /// Number of cells of principal table header (TableHeader).
+        /// </summary>
+        public int TableHeaderCells
+        {
+            get { return SheetUtil.GetTableHeaderCells(_tableHeaders); }
+        }
+
+        /// <summary>
+        /// Firts cell of principal table header (TableHeader).
+        /// </summary>
+        public int FirstHeaderCell { get; set; }
+
+        /// <summary>
+        /// List of Spreadsheets (tabs of Excel/Calc).
+        /// </summary>
+        public IList<Spreadsheet> SpreadsheetList
+        {
+            get { return _spreadsheetList; }
+            set { _spreadsheetList = value; }
+        }
+
+        /// <summary>
+        /// Sheet's name (the value displayed at the Excel/Calc tabs).
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Text displayed at the first merged line (Title).
+        /// </summary>
+        public string MergedTitle { get; set; }
+
+        /// <summary>
+        /// Style and format to apply in the header cells.
+        /// A unique style is applied to all the cells.
+        /// </summary>
+        public HSSFCellStyle HeaderCellStyle { get; set; }
+
+        /// <summary>
+        /// The firts cell of content.
+        /// </summary>
+        public int FirstCell
+        {
+            get { return _firstCell; }
+            set { _firstCell = value; }
+        }
+
+        /// <summary>
+        /// Style and format to apply in the content title.
+        /// </summary>
+        public HSSFCellStyle SpanTitleStyle
+        {
+            get { return _spanTitleStyle; }
+            set { _spanTitleStyle = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Spreadsheet()
+        {
+
+        }
+
+        #endregion
+
+        //public virtual RowStyle RowStyle { get; set; }
+
+        //private IDictionary<string, List<ConditionalFormattingTemplate>> _conditionalFormatDictionary;
+
+        //public virtual IDictionary<string, List<ConditionalFormattingTemplate>> ConditionalFormatList
+        //{
+        //    get { return _conditionalFormatDictionary; }
+        //}
+
+        //public virtual void AddConditionalFormatting(string property, ConditionalFormattingTemplate format)
+        //{
+        //    if (_conditionalFormatDictionary == null)
+        //    {
+        //        _conditionalFormatDictionary = new Dictionary<string, List<ConditionalFormattingTemplate>>();
+        //    }
+
+        //    if (_conditionalFormatDictionary.Keys.Contains(property))
+        //    {
+        //        _conditionalFormatDictionary[property].Add(format);
+        //        _conditionalFormatDictionary[property].Sort(delegate(ConditionalFormattingTemplate a, ConditionalFormattingTemplate b)
+        //        {
+        //            return a.Priority.CompareTo(b.Priority);
+        //        });
+        //    }
+        //    else
+        //    {
+        //        _conditionalFormatDictionary[property] = new List<ConditionalFormattingTemplate>();
+        //        _conditionalFormatDictionary[property].Add(format);
+        //    }
+        //}
+
+        //private SpreadsheetFactory _childSheet;
+        //private ChildSheet _childSheet;
+
+        //public ChildSheet ChildSheet
+        //{
+        //    get { return _childSheet; }
+        //    set { _childSheet = value; }
+        //}
+    }
+}
---
+++ SPFactory/SPFactory.Core/Util/SheetUtil.cs
@@ -0,0 +1,185 @@
+﻿/*
+ * SPFactory is a class library to generate custom spreadsheets using NPOI.
+ * Copyright (C) 2014 Italo Pessoa (italo.pessoa@hotmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using NPOI.HSSF.UserModel;
+using SPFactory.Core.Head;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPFactory.Core.Util
+{
+    /// <summary>
+    /// Utilities methods and properties.
+    /// </summary>
+    public static class SheetUtil
+    {
+        /// <summary>
+        /// Number of cells to span
+        /// </summary>
+        public static int DEFAULT_TITLE_SPAN_SIZE = 10;
+        
+        /// <summary>
+        /// First cell location
+        /// </summary>
+        public static int DEFAULT_FIRST_CELL = 0;
+        private static int tableHeaderRows = 0;
+        private static int tableHeaderRowsAux = 0;
+        private static int _tableHeaderCells = 0;
+        
+        //temporario, a biblioteca padrao nao possui um valor para Datetime
+        /// <summary>
+        /// Datetime cell type identifier
+        /// </summary>
+        public const int CELL_TYPE_DATETIME = 99;
+
+        /// <summary>
+        /// Identify the cell's value type.
+        /// </summary>
+        /// <param name="value">Cell value</param>
+        /// <returns>Value type id.</returns>
+        public static int GetCellType(object value)
+        {
+            Type type = value.GetType();
+
+            switch (type.FullName)
+            {
+                case "System.Int16":
+                case "System.Int32":
+                case "System.Int64":
+                case "System.Double":
+                    return NPOI.HSSF.UserModel.HSSFCell.CELL_TYPE_NUMERIC;
+                case "System.String":
+                    return NPOI.HSSF.UserModel.HSSFCell.CELL_TYPE_STRING;
+                case "System.DateTime":
+                    return SheetUtil.CELL_TYPE_DATETIME;
+                default:
+                    return NPOI.HSSF.UserModel.HSSFCell.CELL_TYPE_STRING;
+            }
+        }
+
+        /// <summary>
+        /// Number of rows of Table Header.
+        /// </summary>
+        /// <param name="tableHeaders">Table Header</param>
+        /// <returns>Number of rows</returns>
+        public static int GetTotalHeaderRows(IList<TableHeader> tableHeaders)
+        {
+            foreach (var item in tableHeaders)
+            {
+                if (item.Cells == null)
+                {
+                    tableHeaderRowsAux++;
+                    if (tableHeaderRowsAux > tableHeaderRows)
+                    {
+                        tableHeaderRows = tableHeaderRowsAux;
+                    }
+                }
+                else if (item.Cells != null)
+                {
+                    tableHeaderRowsAux++;
+                    bool callRecursive = true;
+                    foreach (var internItem in item.Cells)
+                    {
+                        if (internItem.Cells == null || internItem.Cells.Count == 0)
+                        {
+                            callRecursive = false;
+                        }
+                    }
+
+                    if (callRecursive)
+                    {
+                        //teste++;
+                        GetTotalHeaderRows(item.Cells);
+                        if (tableHeaderRowsAux > tableHeaderRows)
+                        {
+                            tableHeaderRows = tableHeaderRowsAux;
+                        }
+                    }
+                    else
+                    {
+                        tableHeaderRowsAux++;
+                        if (tableHeaderRowsAux > tableHeaderRows)
+                        {
+                            tableHeaderRows = tableHeaderRowsAux;
+                        }
+                    }
+                }
+                if (tableHeaderRowsAux > tableHeaderRows)
+                {
+                    tableHeaderRows = tableHeaderRowsAux;
+                }
+                tableHeaderRowsAux = 0;
+            }
+            return tableHeaderRows;
+        }
+
+        /// <summary>
+        /// Number of cells of Table Header.
+        /// </summary>
+        /// <param name="tableHeaders">Table Header</param>
+        /// <returns>Number of cells</returns>
+        public static int GetTableHeaderCells(IList<TableHeader> tableHeaders)
+        {
+            _tableHeaderCells = 0;
+            CountTableHeaderCells(tableHeaders);
+            return _tableHeaderCells;
+        }
+
+        /// <summary>
+        /// Number of cells of Table Header.
+        /// </summary>
+        /// <param name="tableHeaders">Table Header</param>
+        private static void CountTableHeaderCells(IList<TableHeader> tableHeaders)
+        {
+            foreach (var item in tableHeaders)
+            {
+                if (item.Cells == null || item.Cells.Count == 0)
+                {
+                    _tableHeaderCells++;
+                }
+                else
+                {
+                    //tableHeaderCells++;
+                    CountTableHeaderCells(item.Cells);
+                }
+            }
+        }
+
+        //TODO: melhorar essa verificação, uma célula pode não ter um tip definido. se for bool sempre terá um valor. Linha vazia não contém células
+        /// <summary>
+        /// Extension method to verify if a row is empty
+        /// </summary>
+        /// <param name="row">Spreasheet row</param>
+        /// <returns>True if is empty false otherwise.</returns>
+        public static bool IsEmpty(this HSSFRow row)
+        {
+            return row.FirstCellNum < 0;
+        }
+    }
+}
+
+// you need this once (only), and it must be in this namespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// ExtensionAttribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class ExtensionAttribute : Attribute { }
+}
