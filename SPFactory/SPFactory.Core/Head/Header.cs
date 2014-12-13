1fd6010ee4ebac83a70b57f360800255a5ccd188
italopessoa <italo.pessoa@hotmail.com>
2014-12-13 14:19:19 -0300

    Initial structure created
    Created the classes TableHeader and Header to represent the first
    section of the spreadsheet, the filters, and the first data section, the
    table header.

---
+++ SPFactory/SPFactory.Core/Head/Header.cs
@@ -0,0 +1,133 @@
+﻿/*
+ * SPFactory is a class library to generate custom spreadsheets using NPOI.
+ * Copyright (C) 2014 Italo Pessoa (italo.pessoa@hotmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPFactory.Core.Head
+{
+    /// <summary>
+    /// Class to represent the header of the spreadsheet.
+    /// Here you put informations like title, date, filters, etc.
+    /// </summary>
+    public class Header
+    {
+        #region Private Members
+
+        private int DEFAULT_TITLE_SPAN_SIZE = 10;
+
+        private IDictionary<string, object> _filters;
+        private string _title;
+        private string _sheetName;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Values used to get information for generate the spreadsheet
+        /// </summary>
+        public IDictionary<string, object> Filters
+        {
+            get { return _filters; }
+        }
+
+        /// <summary>
+        /// Spreasheet title
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        /// <summary>
+        /// Sheet name. It is important if you have more than one sheet.
+        /// </summary>
+        public string SheetName
+        {
+            get { return _sheetName; }
+            set { _sheetName = value; }
+        }
+
+        /// <summary>
+        /// The number of columns to the sheet span.
+        /// </summary>
+        private int TitleSpan
+        {
+            get { return DEFAULT_TITLE_SPAN_SIZE; }
+            set { DEFAULT_TITLE_SPAN_SIZE = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
+        public Header()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Add new filters to the sheet.
+        /// </summary>
+        /// <param name="key">Value used to identify a filter, i.e. "name", "birth date", "id"...</param>
+        /// <param name="value">Filter's value.</param>
+        /// <returns>Return true if the value was successfully inserted.</returns>
+        public bool AddFilter(string key, object value)
+        {
+            if (ValidadeHeaderValues(key, value))
+            {
+                if (_filters == null)
+                {
+                    _filters = new Dictionary<string, object>();
+                }
+
+                if (!_filters.Keys.Contains(key))
+                {
+                    _filters.Add(key, value);
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Verify a value before insert it in the filters dictionary.
+        /// </summary>
+        /// <param name="key">Filter's name.</param>
+        /// <param name="value">Filter's value</param>
+        /// <returns>Return true the value can be inserted, and false otherwise.</returns>
+        private bool ValidadeHeaderValues(string key, object value)
+        {
+            //TODO: implement filter value verification
+            return true;
+        }
+
+        #endregion        
+    }
+}
---
+++ SPFactory/SPFactory.Core/Head/TableHeader.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * SPFactory is a class library to generate custom spreadsheets using NPOI.
+ * Copyright (C) 2014 Italo Pessoa (italo.pessoa@hotmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPFactory.Core.Head
+{
+    /// <summary>
+    /// Class to represent the principal table header.
+    /// </summary>
+    public class TableHeader
+    {
+        #region Private Members
+
+        private string _text;
+        private IList<TableHeader> _cells = null;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Cell value.
+        /// </summary>
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        /// <summary>
+        /// Used to group TableHeader values.
+        /// </summary>
+        public IList<TableHeader> Cells
+        {
+            get { return _cells; }
+            set { _cells = value; }
+        }
+
+        /// <summary>
+        /// Number of cells to span.
+        /// </summary>
+        public int SpanSize
+        {
+            get
+            {
+                if (_cells != null)
+                {
+                    return CellSpanCount(_cells) - 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public TableHeader()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Add a child cell to the current header.
+        /// </summary>
+        /// <param name="value">Child cell text.</param>
+        /// <returns>A new TableHeader</returns>
+        public TableHeader AddSpanCell(string value)
+        {
+            TableHeader tableHeader = new TableHeader();
+            tableHeader.Text = value;
+            if (_cells == null)
+            {
+                _cells = new List<TableHeader>();
+            }
+            _cells.Add(tableHeader);
+
+
+            return tableHeader;
+        }
+
+        /// <summary>
+        /// Count the number of cells to span.
+        /// </summary>
+        /// <param name="list">List of grouped Table Headers</param>
+        /// <returns>Total of cell span.</returns>
+        private int CellSpanCount(IList<TableHeader> list)
+        {
+            int total = 0;
+            foreach (var item in list)
+            {
+                if (item.Cells == null)
+                {
+                    total++;
+                }
+                else
+                {
+                    total += CellSpanCount(item.Cells);
+                }
+            }
+
+            return total;
+        }
+
+        #endregion
+    }
+}
